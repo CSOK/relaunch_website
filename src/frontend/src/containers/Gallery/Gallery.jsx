75652fd42a57f3baf0ec68febddbe798e46a118f
Victor-Muriithi <vmurithi10@gmail.com>
2023-02-18 22:40:27 +0300

    stories and gallery done

--- src/frontend/src/containers/Gallery/Gallery.jsx
+++ src/frontend/src/containers/Gallery/Gallery.jsx
@@ -4,6 +4,36 @@ const Gallery = () => {
   return (
     <div className="gallery">
       <h1>Gallery Section</h1>
+
+      <div className="content_container">
+        <div className="content_section">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo1d6S7gGw2ul1LEcaOO2wTsHnF3x6FLk-51pauLhOMRbtDlZQ74mceI_cA9SQvXJgFhs&usqp=CAU"
+            alt="People learning"
+          />
+        </div>
+
+        <div className="content_section">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo1d6S7gGw2ul1LEcaOO2wTsHnF3x6FLk-51pauLhOMRbtDlZQ74mceI_cA9SQvXJgFhs&usqp=CAU"
+            alt="People learning"
+          />
+        </div>
+
+        <div className="content_section">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo1d6S7gGw2ul1LEcaOO2wTsHnF3x6FLk-51pauLhOMRbtDlZQ74mceI_cA9SQvXJgFhs&usqp=CAU"
+            alt="People learning"
+          />
+        </div>
+
+        <div className="content_section">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo1d6S7gGw2ul1LEcaOO2wTsHnF3x6FLk-51pauLhOMRbtDlZQ74mceI_cA9SQvXJgFhs&usqp=CAU"
+            alt="People learning"
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/frontend/src/containers/StoriesSection/StoriesSection.jsx
+++ src/frontend/src/containers/StoriesSection/StoriesSection.jsx
@@ -3,7 +3,85 @@ import "./StoriesSection.scss";
 const StoriesSection = () => {
   return (
     <div className="stories">
-      <h1>StoriesSection</h1>
+      <h1 className="story_section_header">Stories Section</h1>
+
+      <div className="content_container">
+        <div className="content_section">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo1d6S7gGw2ul1LEcaOO2wTsHnF3x6FLk-51pauLhOMRbtDlZQ74mceI_cA9SQvXJgFhs&usqp=CAU"
+            alt="People learning"
+          />
+          
+          <p className="date_and_name">25 october|Michael Ngecha</p>
+          
+          <p className="section_header"> LOREM IPSUM DOLOR</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nobis
+            a, non ipsum, perspiciatis dignissimos recusandae eum odit sit nam
+            enim, dolorem ullam magni aliquid. Quis deserunt dolor numquam
+            veritatis.
+          </p>
+          
+          <button>Read More</button>
+        </div>
+
+        <div className="content_section">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo1d6S7gGw2ul1LEcaOO2wTsHnF3x6FLk-51pauLhOMRbtDlZQ74mceI_cA9SQvXJgFhs&usqp=CAU"
+            alt="People learning"
+          />
+          
+          <p className="date_and_name">25 october|Michael Ngecha</p>
+          
+          <p className="section_header"> LOREM IPSUM DOLOR</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nobis
+            a, non ipsum, perspiciatis dignissimos recusandae eum odit sit nam
+            enim, dolorem ullam magni aliquid. Quis deserunt dolor numquam
+            veritatis.
+          </p>
+          
+          <button>Read More</button>
+        </div>
+
+        <div className="content_section">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo1d6S7gGw2ul1LEcaOO2wTsHnF3x6FLk-51pauLhOMRbtDlZQ74mceI_cA9SQvXJgFhs&usqp=CAU"
+            alt="People learning"
+          />
+          
+          <p className="date_and_name">25 october|Michael Ngecha</p>
+          
+          <p className="section_header"> LOREM IPSUM DOLOR</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nobis
+            a, non ipsum, perspiciatis dignissimos recusandae eum odit sit nam
+            enim, dolorem ullam magni aliquid. Quis deserunt dolor numquam
+            veritatis.
+          </p>
+          
+          <button>Read More</button>
+        </div>
+
+        <div className="content_section">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo1d6S7gGw2ul1LEcaOO2wTsHnF3x6FLk-51pauLhOMRbtDlZQ74mceI_cA9SQvXJgFhs&usqp=CAU"
+            alt="People learning"
+          />
+          
+          <p className="date_and_name">25 october|Michael Ngecha</p>
+          
+          <p className="section_header"> LOREM IPSUM DOLOR</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nobis
+            a, non ipsum, perspiciatis dignissimos recusandae eum odit sit nam
+            enim, dolorem ullam magni aliquid. Quis deserunt dolor numquam
+            veritatis.
+          </p>
+          
+          <button>Read More</button>
+        </div>
+      </div>
     </div>
   );
 };
